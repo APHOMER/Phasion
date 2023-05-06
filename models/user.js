43d41f3780a3d8dc91c2b9d853459085bc9c0dac
Aphomer <aaphomer@gmail.com>
2023-05-06 21:31:49 +0100

    validateUse user errorr

--- models/user.js
+++ models/user.js
@@ -45,14 +45,14 @@ const UserSchema = new Schema({
 }
 )
 
-function validateUser(user) {
-    const schema = {
-        name: Joi.string().min(5).max(15).required(),
-        email: Joi.string().trim().lowercase().min(10).max(30).required(),
-    };
+// function validateUser(user) {
+//     const schema = {
+//         name: Joi.string().min(5).max(15).required(),
+//         email: Joi.string().trim().lowercase().min(10).max(30).required(),
+//     };
 
-    return Joi.validate(user, schema);
-}
+//     return Joi.validate(user, schema);
+// }
 
 
 UserSchema.plugin(passportLocalMongoose)
