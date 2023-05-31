959a744d50a514a6382fefcfb9218b7e966e03ec
Aphomer <aaphomer@gmail.com>
2023-05-31 09:07:19 +0100

    ........

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
-}
+// if (process.env.NODE_ENV !== "production") {
+//     require('dotenv').config();
+// }
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 
 const express = require('express');
