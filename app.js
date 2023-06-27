169f17b6c8f6bce0a850726a53938b3c06b3ba8b
Aphomer <aaphomer@gmail.com>
2023-06-27 14:01:37 +0100

    uncommented and deleted dotenv

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
-// require('dotenv').config()
-// console.log(process.env) 
-
 
 const express = require('express');
 const app = express();
